68768c3c666889a86610dff1a8ffa674ea74b08f
Akib Ashfaq <akibash.dev@gmail.com>
2025-04-21 02:19:14 +0600

    LeetCode Solveset
    Category : Easy 👍

---
+++ 20.Valid Parentheses.cpp
@@ -0,0 +1,37 @@
+#include <stack>
+#include <string>
+
+class Solution {
+public:
+    bool isValid(std::string s) {
+        std::stack<char> st;
+
+        for (char c : s) {
+            switch (c) {
+                case '(': case '{': case '[':
+                    st.push(c);
+                    break;
+
+                case ')':
+                    if (st.empty() || st.top() != '(') return false;
+                    st.pop();
+                    break;
+
+                case '}':
+                    if (st.empty() || st.top() != '{') return false;
+                    st.pop();
+                    break;
+
+                case ']':
+                    if (st.empty() || st.top() != '[') return false;
+                    st.pop();
+                    break;
+
+                default:
+                    return false;
+            }
+        }
+
+        return st.empty();
+    }
+};
