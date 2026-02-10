ff574e80b44a7c076555a09d11b72e486eac1201
AkibAshfaq <akibash.dev@gmail.com>
2026-02-11 05:15:00 +0600

    LeetCode Solve

---
+++ Easy/Tree/94. Binary Tree Inorder Traversal.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<vector>
+#include<stack>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> inorder;
+        stack<TreeNode*> stk;
+        TreeNode* curr = root;
+        while(curr!=nullptr || !stk.empty()){
+            while(curr!=nullptr){
+                stk.push(curr);
+                curr=curr->left;
+            }
+            curr=stk.top();
+            stk.pop();
+            inorder.push_back(curr->val);
+            curr=curr->right;
+        }
+        return inorder;
+    }
+};
